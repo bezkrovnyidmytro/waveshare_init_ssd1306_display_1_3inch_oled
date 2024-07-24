9e2285c0798f9c10668d92debaef8ba5dee6defc
bezkrovnyidmytro <bezkrovnyidmytro@gmail.com>
2024-07-24 18:30:41 +0300

    Code refactor

--- init_display/init_display.py
+++ init_display/init_display.py
@@ -10,7 +10,6 @@ import psutil
 import struct
 import smbus
 import gpiod
-import re
 from PIL import Image, ImageDraw, ImageFont
 
 # Configure logging
@@ -24,6 +23,11 @@ PLD_PIN = 6
 ADDRESS = 0x36
 SLEEP_TIME_AC = 1
 SLEEP_TIME_BAT = 10
+
+BAT_THRESHOLD_HIGH = 70
+BAT_THRESHOLD_MEDIUM = 50
+BAT_THRESHOLD_LOW = 30
+BAT_THRESHOLD_CRIT = 15
 
 # Directories
 script_path = os.path.realpath(__file__)
@@ -40,6 +44,7 @@ font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), FONT_SIZE)
 disp = OLED_1in3.OLED_1in3()
 disp.init_display()
 
+
 def read_word(bus, register):
     """Read a word from the I2C device and return the swapped value."""
     try:
@@ -49,43 +54,54 @@ def read_word(bus, register):
         logging.error(f"Failed to read from I2C: {e}")
         return 0  # Return 0 or a sensible default in case of failure
 
+
 def read_voltage(bus):
     """Calculate and return the voltage from the battery."""
     return read_word(bus, 2) * 1.25 / 1000 / 16
+
 
 def read_capacity(bus):
     """Calculate and return the battery capacity."""
     return round(read_word(bus, 4) / 256, 2)
 
-def get_battery_info(bus, ac_power_state):
-    """Return a formatted string with battery capacity and power state."""
-    capacity = int(read_capacity(bus))
-    power_state = "+" if ac_power_state == 1 else "-"
-    return f'{power_state}{capacity}%'
 
-def get_system_info():
-    """Fetch current time, CPU temperature, CPU and memory usage."""
-    current_time = datetime.datetime.now().strftime('%d-%m-%y %H:%M')
-    cpu_temp = subprocess.check_output(['vcgencmd', 'measure_temp']).decode("utf-8").strip().replace('temp=', 't: ').upper()
+def get_battery_info(ac_power_state, capacity):
+    """Return a formatted string with battery capacity and power state."""
+    return f'{"+" if ac_power_state == 1 else "-"}{capacity}%{get_battery_capacity_label(capacity)}'
+
+
+def get_cpu_temp():
+    """CPU temperature."""
+    cpu_temp =  subprocess.check_output(['vcgencmd', 'measure_temp']).decode("utf-8").strip().replace('temp=', 't: ').upper()
+    return cpu_temp
+
+
+def get_cpu_usage():
+    """CPU usage."""
     cpu_usage = f'CPU: {psutil.cpu_percent(interval=None)}%'
+    return cpu_usage
+
+
+def get_mem_usage():
+    """Fetch memory usage."""
     mem_usage = f'MEM: {psutil.virtual_memory().percent}%'
-    return current_time, cpu_temp, cpu_usage, mem_usage
+    return mem_usage
+
 
 def get_disk_usage():
     return str(round(psutil.disk_usage('/').used / (1024.0 ** 3), 1))
 
-def draw_display(current_time, cpu_temp, cpu_usage, mem_usage, battery_status, network_info, disk_usage, script_info):
+
+def draw_display(current_time, cpu_temp, cpu_usage, mem_usage, battery_status, network_info, disk_usage):
     """Draw system information on the OLED display."""
     image = Image.new(mode='1', size=(disp.width, disp.height), color="WHITE")
     draw = ImageDraw.Draw(image)
-
-    draw.text((LEFT_PADDING, 0), f"{current_time} | {battery_status}", font=font, fill=0)
-    draw.text((LEFT_PADDING, FONT_SIZE), f"{cpu_usage} | {cpu_temp}", font=font, fill=0)
-    draw.text((LEFT_PADDING, 2 * FONT_SIZE), f'{mem_usage} | D: {disk_usage}G', font=font, fill=0)
-    draw.text((LEFT_PADDING, 3 * FONT_SIZE), network_info, font=font, fill=0)
-    draw.text((LEFT_PADDING, 4 * FONT_SIZE), script_info, font=font, fill=0)
-
+    draw.text((LEFT_PADDING, 0), f"{current_time} | {battery_status}", font=font)
+    draw.text((LEFT_PADDING, FONT_SIZE), f"{cpu_usage} | {cpu_temp}", font=font)
+    draw.text((LEFT_PADDING, 2 * FONT_SIZE), f'{mem_usage} | D: {disk_usage}G', font=font)
+    draw.text((LEFT_PADDING, 3 * FONT_SIZE), network_info, font=font)
     return image
+
 
 def create_pid_file():
     """Create or remove a PID file for the script."""
@@ -99,7 +115,8 @@ def create_pid_file():
             f.write(pid)
     except Exception as e:
         logging.error(f"Error creating PID file: {e}")
-        
+
+
 def validate_ip(s):
     a = s.split('.')
     if len(a) != 4:
@@ -110,20 +127,12 @@ def validate_ip(s):
         i = int(x)
         if i < 0 or i > 255:
             return False
-    return True    
-        
-def get_ip_address():
-    ipaddress = psutil.net_if_addrs()['wlan0'][0].address
-    return replace_ip(ipaddress) if validate_ip(ipaddress) else ''
+    return True
+
 
 def get_network_info():
-    ip = get_ip_address()
-    return (f'wlan0: {ip}' if ip else 'wlan0: n/a').upper()
+    return (f'eth0: {psutil.net_if_addrs()["eth0"][0].address}' if psutil.net_if_stats()['eth0'].isup else 'eth0: n/a').upper()
 
-def replace_ip(ip_address):
-    # Use a regular expression to match the first two numbers in the IP address
-    replaced_ip = re.sub(r'^\d+\.\d+', '*.*', ip_address)
-    return replaced_ip
 
 def get_script_info(process):
     # Get memory usage in bytes
@@ -131,6 +140,34 @@ def get_script_info(process):
     # Get CPU usage in percentage
     cpu_usage = process.cpu_percent(interval=None)  # interval can be adjusted
     return f'M: {memory_usage / (1024 * 1024):.1f} MB | CPU: {cpu_usage}%'
+
+
+def get_battery_capacity_label(value):
+    match value:
+        case _ if value >= BAT_THRESHOLD_HIGH:
+            return "H"
+        case _ if value < BAT_THRESHOLD_HIGH and value >= BAT_THRESHOLD_MEDIUM:
+            return "M"
+        case _ if value < BAT_THRESHOLD_MEDIUM and value >= BAT_THRESHOLD_LOW:
+            return "L"
+        case _ if value < BAT_THRESHOLD_LOW and value >= BAT_THRESHOLD_CRIT:
+            return "C"
+    return "!"
+
+
+def check_shutdown_status(ac_status, battery_capacity):
+    return ac_status != 1 and BAT_THRESHOLD_LOW > battery_capacity >= BAT_THRESHOLD_CRIT
+
+
+def make_shutdown(ac_power_state, capacity):
+    logging.error(f"{get_current_datetime()}: AC {ac_power_state} battery capacity is {capacity}, performing a shutdown...")
+    os.system("shutdown now -h")
+    exit()
+
+
+def get_current_datetime():
+    return datetime.datetime.now().strftime('%d-%m-%y %H:%M')
+
 
 def main():
     create_pid_file()
@@ -141,23 +178,25 @@ def main():
     chip = gpiod.Chip('gpiochip4')
     pld_line = chip.get_line(PLD_PIN)
     pld_line.request(consumer="PLD", type=gpiod.LINE_REQ_DIR_IN)
-    process = psutil.Process(os.getpid())
+
+    disk_usage = get_disk_usage()
 
     while True:
         ac_power_state = pld_line.get_value()
-        battery_info = get_battery_info(bus, ac_power_state)
-        current_time, cpu_temp, cpu_usage, mem_usage = get_system_info()
+        capacity = int(read_capacity(bus))
+        if check_shutdown_status(ac_power_state, capacity) == True:
+            make_shutdown()
+        battery_info = get_battery_info(ac_power_state, capacity)
+        current_time = get_current_datetime()
+        cpu_temp = get_cpu_temp()
+        cpu_usage = get_cpu_usage()
+        mem_usage = get_mem_usage()
         network_info = get_network_info()
-        sleep_time = SLEEP_TIME_AC if ac_power_state else SLEEP_TIME_BAT
-        disk_usage = get_disk_usage()
-        script_info = get_script_info(process)
 
-        image = draw_display(current_time, cpu_temp, cpu_usage, mem_usage, battery_info, network_info, disk_usage, script_info)
+        image = draw_display(current_time, cpu_temp, cpu_usage, mem_usage, battery_info, network_info, disk_usage)
         disp.show_image(disp.get_buffer(image))
+        time.sleep(SLEEP_TIME_AC if ac_power_state else SLEEP_TIME_BAT)
 
-        del image, current_time, cpu_temp, cpu_usage, mem_usage, battery_info, network_info, disk_usage, script_info
-
-        time.sleep(sleep_time)
 
 if __name__ == "__main__":
     main()
